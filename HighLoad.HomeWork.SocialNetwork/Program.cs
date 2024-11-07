40b8606f6038b315a7134c80b49075bc538d77b6
Безруков Глеб <g.bezrukov@company.moisport.ru>
2024-11-07 12:09:24 +0300

    master: Инфраструктурные доработки

--- HighLoad.HomeWork.SocialNetwork/Program.cs
+++ HighLoad.HomeWork.SocialNetwork/Program.cs
@@ -1,16 +1,86 @@
 using HighLoad.HomeWork.SocialNetwork.Interfaces;
 using HighLoad.HomeWork.SocialNetwork.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Чтение конфигурации для JWT из appsettings.json
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
 // Добавление сервисов в контейнер
 builder.Services.AddControllers();
+
+// Добавление поддержки Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Social Network API", Version = "v1" });
+
+    // Настройка JWT авторизации в Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Введите 'Bearer' [пробел] и ваш токен JWT в поле ниже.\n\nПример: 'Bearer eyJhbGciOiJIUzI1NiIsIn...'"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Регистрация пользовательских сервисов
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasherService>();
 
+// Настройка аутентификации с использованием JWT Bearer
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!))
+        };
+    });
+
+// Добавление авторизации
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// Включение Swagger только в режиме разработки
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Social Network API V1");
+        c.RoutePrefix = "swagger";
+    });
+}
 
 // Конвейер обработки запросов
 app.UseAuthentication();
